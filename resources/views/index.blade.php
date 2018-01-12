ab369330acb09195ebfdfd1f64f5d2d0de67123f
Knopp Konstantin <k.knopp@das-unikat.com>
2018-01-12 18:38:09 +0100

    main view

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,1 +1,17 @@
-all-import
+<!doctype html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="token" content="{{ csrf_token() }}">
+    <base href="{{ url(config_path('all-import.route.prefx')) . '/' }}">
+    <link type="text/css" rel="stylesheet" href="{{ route('all-import.file', ['file' => 'materialize.min.css']) }}">
+</head>
+<body>
+<div id="app">
+    All Import
+</div>
+<script type="text/javascript" src="{{ route('all-import.file', ['file' => 'materialize.min.js']) }}"></script>
+</body>
+</html>
