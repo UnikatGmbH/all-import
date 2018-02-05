db8dfe0ff768d61d0ef8ce0715e7ae092fe3f467
Konstantin Knopp <k.knopp@das-unikat.com>
2018-02-05 16:17:58 +0100

    handle identical filenames

--- src/Http/Controllers/UploadController.php
+++ src/Http/Controllers/UploadController.php
@@ -3,13 +3,20 @@
 namespace Unikat\AllImport\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class UploadController extends Controller
 {
     
     public function upload(Request $request)
     {
-        return $request->file->storeAs('all-import', $request->file->getClientOriginalName(), 'local');
+        $fileName = $request->file->getClientOriginalName();
+    
+        if(Storage::exists('all-import/' . $request->file->getClientOriginalName())) {
+            $fileName = '2_' . $fileName; // TODO: es muss für jede vorhandene Datei mit gleichen Namen hochgezählt werden
+        }
+    
+        return $request->file->storeAs('all-import', $fileName, 'local');
         
     }
 }
